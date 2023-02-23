baeda68882763c8fc8e0c96a0a106557493f3ba4
Jake Soenneker <jake@soenneker.com>
2023-02-22 20:19:58 -0600

    Additional method

--- src/StreamExtension.cs
+++ src/StreamExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Diagnostics.Contracts;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Soenneker.Extensions.Stream;
 
@@ -10,5 +12,16 @@ public static class StreamExtension
     public static void ToStart(this System.IO.Stream stream)
     {
         stream.Seek(0, SeekOrigin.Begin);
+    }
+
+    /// <summary>
+    /// Does not close the stream. Reads via a StreamReader. Does not reset the position of the stream once done
+    /// </summary>
+    [Pure]
+    public static async ValueTask<string> ToStr(this System.IO.Stream value)
+    {
+        var reader = new StreamReader(value);
+        string result = await reader.ReadToEndAsync();
+        return result;
     }
 }
